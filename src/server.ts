cecda0b3f4d22f85d86346057550eee1a20e1646
wiliamvj <wiliamjoaquim@gmail.com>
2022-07-26 14:16:37 -0300

    handling errors with express

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { NextFunction, Response, Request } from 'express';
+import 'express-async-errors';
 import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({ message: err.message });
+  }
+
+  return res.status(500).json({
+    message: 'Interna server error',
+  });
+});
 
 app.listen(3001, () => console.log(`🔥 Server is running on port 3001`));
