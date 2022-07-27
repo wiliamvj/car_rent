119af29f3c6c2fab3a30d5237ad859902936bdd5
wiliamvj <wiliamjoaquim@gmail.com>
2022-07-27 11:05:14 -0300

    init documetation with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express, { NextFunction, Response, Request } from 'express';
+import swaggerUi from 'swagger-ui-express';
 import 'express-async-errors';
-import { routes } from './routes';
 
+import swaggerConfig from './swagger.json';
+
+import { routes } from './routes';
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
