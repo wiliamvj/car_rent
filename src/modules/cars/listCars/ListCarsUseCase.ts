6a458cc69acbfc36fa48a41f0d1f1ccde1160b9c
wiliamvj <wiliamjoaquim@gmail.com>
2022-07-26 22:46:28 -0300

    update to list cars

--- src/modules/cars/listCars/ListCarsUseCase.ts
+++ src/modules/cars/listCars/ListCarsUseCase.ts
@@ -5,8 +5,10 @@ export class ListCarsUseCase {
     const cars = await prisma.cars.findMany({
       select: {
         id: true,
-        car_name: true,
-        brand: true,
+        title: true,
+        specs: true,
+        in_rent: true,
+        until_rent: true,
         price: true,
         created_at: true,
         id_user_rent: true,
